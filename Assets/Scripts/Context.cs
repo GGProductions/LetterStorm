4b6bc39533244de47a0ba769425c61d69ade540b
jtrinhx33 <jtrinh@bu.edu>
2014-10-04 03:08:42 -0400

    Add basic HUD and Context Script [develop]

---
+++ Assets/Scripts/Context.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Context : MonoBehaviour {
+
+    // Static variables to track in-game information
+    private static int PlayerLives = 3;
+    // JT TODO: Create class to hold letters needed and how many of them
+    // Using class (above), have a variable that keeps track of each letter and how many of them have been collected (i.e. put in array)
+
+}
---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+
+    private GUIText Lives;
+
+    /// <summary>
+    /// Method that updates HUD once every frame.
+    /// </summary>
+    void OnGUI()
+    {
+
+        GUI.Label(new Rect(10, 10, 250, 20), "Score: ");
+        GUI.Label(new Rect(10, 30, 250, 20), "Lives: " + Player.Lives.ToString());
+        GUI.Label(new Rect(10, 50, 250, 20), "Letters Collected: ");
+        GUI.Label(new Rect(10, 70, 250, 20), "Letters Needed: ");
+
+        Lives.text = "Lives: " + Player.Lives;
+    }
+}
