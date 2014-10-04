0f2f85ff81568c4f17dc38059db2e32c45703059
jamraygo <jamraygo@bu.edu>
2014-10-04 18:44:28 +0200

    begin menu changes

--- Assets/Scripts/Lose.cs
+++ Assets/Scripts/Lose.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Lose : MonoBehaviour
@@ -20,12 +20,26 @@ public class Lose : MonoBehaviour
     {
         GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
 
-        if (Input.anyKeyDown)
+        if(GUI.Button(new Rect(70, Screen.height / 2 + 25, 200, 20), "Insert Coin, to continue playing"))
         {
+            Application.LoadLevel(1);
+            Player.Lives = 3;
+        }
+        
+        if(GUI.Button(new Rect(123, Screen.height / 2 + 50, 90, 20), "Restart Level"))
+        {
+            Application.LoadLevel(1);
             Player.Score = 0;
             Player.Lives = 3;
             Player.Missed = 0;
-            Application.LoadLevel(1);
+        }
+        
+        if(GUI.Button(new Rect(130, Screen.height / 2 + 75, 75, 20), "Main Menu"))
+        {
+            Application.LoadLevel(0);
+            Player.Score = 0;
+            Player.Lives = 3;
+            Player.Missed = 0;
         }
     }
 
