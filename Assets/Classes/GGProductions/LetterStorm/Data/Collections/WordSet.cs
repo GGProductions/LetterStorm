4316dda99b2a7166415be828b11789f144427c4a
davelustig <davelustig@users.noreply.github.com>
2014-10-04 02:25:24 -0400

    Add Data Structures for Lessons and Saving
    Add the data structures representing the word sets users can be tested
    on (ie Lessons)
    Add data structure representing all data that will be saved
    Add save and load functions

---
+++ Assets/Classes/GGProductions/LetterStorm/Data/Collections/WordSet.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GGProductions.LetterStorm.Data.Collections
+{
+    /// <summary>
+    /// Represents a List of Words the user can be tested on
+    /// </summary>
+    [Serializable]
+    public class WordSet : List<Word>
+    {
+        #region Private Variables ---------------------------------------------
+        /// <summary>Random number generator</summary>
+        /// <remarks>This is stored as part of the class instead of within a 
+        /// function so that it produces different numbers when used multiple 
+        /// times in a row, and to reduce initialization time</remarks>
+        private static Random rand = null;
+        #endregion Private Variables ------------------------------------------
+
+        #region Constructors --------------------------------------------------
+        /// <summary>
+        /// Represents a List of Words the user can be tested on
+        /// </summary>
+        public WordSet(): base()
+        {
+            
+        }
+        #endregion Constructors -----------------------------------------------
+        
+        #region Psuedo-Enumerators --------------------------------------------
+        /// <summary>
+        /// Retrieve the next Word from this Word Set.  The Word will be the 
+        /// first untested word in the sorted List.
+        /// </summary>
+        /// <exception cref="NoUntestedWordsException">Thrown if the WordSet
+        /// does not contain any words the user has not been tested against</exception>
+        public Word GetNextWord()
+        {
+            // Create the query to retrieve all Words that the user has not 
+            // been tested on, ordered from shortest to longest
+            var queryUntestedWords = from Word w in this
+                                     where w.Tested == false
+                                     orderby w.Text ascending
+                                     select w;
+
+            // If the user has not already been tested on all Words in the WordSet...
+            if (queryUntestedWords.Count() > 0)
+            {
+                // Select the first Word from the sorted result set
+                Word nextWord = queryUntestedWords.ElementAt(0);
+
+                // Find that word in the original List and mark it as tested so it is not returned again
+                int indexOfNextWord = this.IndexOf(nextWord);
+                this[indexOfNextWord].Tested = true;
+
+                // Return the next word to the caller
+                return nextWord;
+            }
+            // Else if the user has already been tested against all words 
+            // in the WordSet, throw a NoUntestedWordsException
+            else
+                throw new NoUntestedWordsException();
+        }
+
+        /// <summary>
+        /// Retrieve a random word from this Word Set.  The word will be taken
+        /// from a subset composing of all words of the shortest length.
+        /// </summary>
+        /// <exception cref="NoUntestedWordsException">Thrown if the WordSet
+        /// does not contain any words the user has not been tested against</exception>
+        public Word GetRandomWord()
+        {
+            // Create the query to retrieve all words that the user has not 
+            // been tested on, ordered from shortest to longest
+            var queryUntestedWords = from Word w in this
+                                     where w.Tested == false
+                                     orderby w.Text ascending
+                                     select w;
+
+            // If the user has not already been tested on all words in the WordSet...
+            if (queryUntestedWords.Count() > 0)
+            {
+                // Find the length of the shortest word in the result set
+                int shortestUntestedWordLength = queryUntestedWords.ElementAt(0).Text.Length;
+
+                // Create a subquery to retrieve all the shortest words
+                var queryShortestWords = from Word w in queryUntestedWords
+                                         where w.Text.Length == shortestUntestedWordLength
+                                         select w;
+
+                // If the random number generator has not been initialized, do so
+                if (rand == null)
+                    rand = new Random();
+
+                // Select a random word from the shortest-words result set
+                Word randomWord = queryShortestWords.ElementAt(rand.Next(queryShortestWords.Count()));
+
+                // Find that word in the original List and mark it as tested so it is not returned again
+                int indexOfRandomWord = this.IndexOf(randomWord);
+                this[indexOfRandomWord].Tested = true;
+
+                // Return the random shortest word to the caller
+                return randomWord;
+            }
+            // Else if the user has already been tested against all words 
+            // in the WordSet, throw a NoUntestedWordsException
+            else
+                throw new NoUntestedWordsException();
+        }
+        #endregion Psuedo-Enumerators -----------------------------------------
+
+        #region State Functions -----------------------------------------------
+        /// <summary>
+        /// Determine if the WordSet contains words that 
+        /// the user has not been tested on
+        /// </summary>
+        public bool ContainsUntestedWords()
+        {
+            // Create a query to retrieve all words that the user has 
+            // not been tested on
+            var queryUntestedWords = from Word w in this
+                                     where w.Tested == false
+                                     select w;
+            // Return whether any such words still exist
+            return queryUntestedWords.Count() > 0;
+        }
+
+        /// <summary>
+        /// Reset all Words in this WordSet to be untested
+        /// </summary>
+        /// <remarks>WARNING: Only call this before starting the game play;
+        /// calling during a level or between levels may result in the
+        /// user being retested on the same words during the same session</remarks>
+        public void ResetAllWordsToUntested()
+        {
+            for (int i = 0; i < this.Count; i++)
+                this[i].Tested = false;
+        }
+        #endregion State Functions --------------------------------------------
+    }
+}
---
+++ Assets/Classes/GGProductions/LetterStorm/Data/Word.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace GGProductions.LetterStorm.Data
+{
+    /// <summary>
+    /// Represents a word that a user can be tested against, 
+    /// as well as its associated hint and metadata
+    /// </summary>
+    [Serializable]
+    public class Word: IComparable
+    {
+        #region Private Variables ---------------------------------------------
+        // Create the private variables that the this class's 
+        //  properties will use to store their data
+        private string _text;
+        private string _hint;
+        
+        // Do not include this field in the save file
+        [NonSerialized]
+        private bool _tested;
+        #endregion Private Variables ------------------------------------------
+
+        #region Properties ----------------------------------------------------
+        /// <summary>
+        /// The text of the word
+        /// </summary>
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+            }
+        }
+
+        /// <summary>
+        /// The hint associated with the word
+        /// </summary>
+        public string Hint
+        {
+            get
+            {
+                return _hint;
+            }
+            set
+            {
+                _hint = value;
+            }
+        }
+
+        /// <summary>
+        /// Flag indicating whether the user was tested on how to spell this word
+        /// </summary>  
+        public bool Tested
+        {
+            get
+            {
+                return _tested;
+            }
+            set
+            {
+                _tested = value;
+            }
+        }
+        #endregion Properties -------------------------------------------------
+
+        #region Constructors --------------------------------------------------
+        /// <summary>
+        /// Represents a word that a user can be tested against, 
+        /// as well as its associated hint and metadata
+        /// </summary>
+        public Word()
+        {
+        }
+
+        /// <summary>
+        /// Represents a word that a user can be tested against, 
+        /// as well as its associated hint and metadata
+        /// </summary>
+        /// <param name="text">The text of the word the user should be tested on</param>
+        public Word(string text)
+        {
+            _text = text;
+        }
+
+        /// <summary>
+        /// Represents a word that a user can be tested against, 
+        /// as well as its associated hint and metadata
+        /// </summary>
+        /// <param name="text">The text of the word the user should be tested on</param>
+        /// <param name="hint">The hint associated with the word</param>
+        public Word(string text, string hint)
+        {
+            _text = text;
+            _hint = hint;
+        }
+        #endregion Constructors -----------------------------------------------
+
+        #region Overridden Methods --------------------------------------------
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return _text + "||" + _hint;
+            //return base.ToString();
+        }
+        #endregion Overridden Methods -----------------------------------------
+
+        #region IComparable Methods -------------------------------------------
+        /// <summary>
+        /// Compares the current instance with another object of the same type 
+        /// and returns an integer that indicates whether the current instance 
+        /// precedes, follows, or occurs in the same position in the sort order 
+        /// as the other object
+        /// </summary>
+        /// <param name="obj">An object to compare with this instance.</param>
+        /// <returns>-1 = this is less than obj; 0 = this is equal to obj;
+        /// 1 = this is greater than obj</returns>
+        int IComparable.CompareTo(object obj)
+        {
+            if (obj == null)    // If the object to compare this instance to is null,
+                return 1;       // note that this instance is greater than the object
+
+            // Attempt to convert obj to the same type as this, and compare each's 
+            // Text property to determine in what order they should be sorted
+            Word otherWord = obj as Word;
+            return this.Text.CompareTo(otherWord.Text);
+        }
+        #endregion IComparable Methods ----------------------------------------
+    }
+}
