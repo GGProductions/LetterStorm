08d7f3a63462dada7fe8dcd819dadd2fdea9a24d
jtrinhx33 <jtrinh@bu.edu>
2014-10-05 01:04:48 -0400

    Add basic inventory of letters. Press [L] to increment an A to the inventory

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -6,9 +6,12 @@ public class HUD : MonoBehaviour {
     #region Private Variables ---------------------------------------------
     // Private variables representing components of the HUD
     private GUIText LabelLives;
+    GUIStyle InventoryStyle;
     private int InventoryBoxWidth = (int)(Screen.width * .75);
-    private int InventoryBoxHeight = 30;
+    private int InventoryItemBoxWidth = 25;
+    private int InventoryItemBoxHeight = 20;
     private int InventoryBoxBottomMargin = 5;
+    private int InventoryBoxSideMargin = 5;
 
     // If game is paused or not paused
     private bool isPaused;
@@ -20,6 +23,7 @@ public class HUD : MonoBehaviour {
     void Start()
     {
         isPaused = false;
+        InventoryStyle = new GUIStyle();
     }
 
     /// <summary>
@@ -28,17 +32,86 @@ public class HUD : MonoBehaviour {
     /// </summary>
     void OnGUI()
     {
-        //GUI.Label(new Rect(10, 10, 250, 20), "Score: ");
-        //GUI.Label(new Rect(10, 30, 250, 20), "Lives: " + Context.PlayerLives.ToString());
-        //GUI.Label(new Rect(10, 50, 250, 20), "Letters Collected: ");
-        //GUI.Label(new Rect(10, 70, 250, 20), "Letters Needed: ");
-
         GUILayout.Box("Lives: " + Context.PlayerLives.ToString());
         GUILayout.Box("Letters Collected: " + Context.PlayerInventory.TotalCollectedLetters);
         GUILayout.Box("Letters Needed: ");
-        GUI.Box(new Rect(Screen.width / 2 - InventoryBoxWidth / 2, Screen.height - InventoryBoxHeight - InventoryBoxBottomMargin, InventoryBoxWidth, InventoryBoxHeight), Context.PlayerInventory.A.Count.ToString());
-
         
+        InventoryWindow();
+        
+    }
+
+    /// <summary>
+    /// Method that updates HUD once every frame
+    /// Displays game, player information, and player inventory
+    /// </summary>
+    void InventoryWindow()
+    {
+
+        GUILayout.BeginArea(new Rect(
+            Screen.width / 2 - InventoryItemBoxWidth * 31 / 2,                          // X start position
+            Screen.height - InventoryItemBoxHeight * 3 - InventoryBoxBottomMargin,      // Y start position
+            InventoryItemBoxWidth * 31,                                                 // Width
+            InventoryItemBoxHeight * 3));                                               // Height
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Button("A", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("B", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("C", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("D", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("E", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("F", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("G", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("H", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("I", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("J", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("K", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("L", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("M", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("N", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("O", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("P", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("Q", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("R", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("S", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("T", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("U", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("V", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("W", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("X", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("Y", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button("Z", GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Button(Context.PlayerInventory.A.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.B.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.C.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.D.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.E.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.F.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.G.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.H.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.I.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.J.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.K.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.L.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.M.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.N.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.O.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.P.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.Q.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.R.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.S.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.T.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.U.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.V.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.W.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.X.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.Y.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.Button(Context.PlayerInventory.Z.Count.ToString(), GUILayout.Height(InventoryItemBoxHeight), GUILayout.Width(InventoryItemBoxWidth));
+        GUILayout.EndHorizontal();
+
+        GUILayout.EndArea();
     }
 
     /// <summary>
