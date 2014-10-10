29e93817932ef73a3d718680adc781b96eabee26
nablam <lamribenn@gmail.com>
2014-10-09 21:02:10 -0400

    revert from accidental deletion

---
+++ Assets/Scripts/BossShootInterval.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossShootInterval : MonoBehaviour {
+    public GameObject theShot;
+    public float fireRate = 1.8f;
+
+
+
+
+    IEnumerator Start()
+    {
+        while (gameObject)
+        {
+
+            Vector3 here = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+            Instantiate(theShot, here, transform.rotation);
+
+            yield return new WaitForSeconds(fireRate);
+        }
+
+
+    }
+
+
+}
---
+++ Assets/Scripts/BossV1.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossV1 : MonoBehaviour {
+
+	
+	private Vector3 planeLoc;
+	private Vector3 rotShift;
+	public Quaternion angleNeeded;
+	
+	// Use this for initialization
+	void Start () {
+		rotShift = new Vector3(0, 90, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//	 planeLoc =GameObject.Find("playerFlatPrefab").transform.position;
+		planeLoc =GameObject.Find("AlbertPrefab").transform.position;
+		
+		//Debug.Log(planeLoc);
+
+	   // transform.LookAt(planeLoc);
+		
+		float damp=1.8f;
+
+   //     angleNeeded = Quaternion.LookRotation(planeLoc - new Vector3(transform.position.x *-1 , transform.position.y, transform.position.z));
+		angleNeeded = Quaternion.LookRotation(planeLoc - transform.position);
+		
+		transform.rotation= Quaternion.Slerp(transform.rotation, angleNeeded, Time.deltaTime*damp);
+		
+		
+		
+		
+		
+	}
+}
+
+
+
+
