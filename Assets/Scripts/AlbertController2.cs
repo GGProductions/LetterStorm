6fb9ab21d061fbc00780a6c30c1c8b059d5742c2
nablam <lamribenn@gmail.com>
2014-11-02 21:52:13 -0500

    boss normal texture implemented

--- Assets/Scripts/AlbertController2.cs
+++ Assets/Scripts/AlbertController2.cs
@@ -40,15 +40,18 @@ public class AlbertController2 : MonoBehaviour {
         LetterBulletname = string.Empty;
         mytransform = this.transform;
         myAmmoSpawn = transform.Find("AmmoSpawnPoint");
-        
-        animation.AddClip(idleAnimationClip, "idleing");
-        animation.AddClip(fallAnimationClip, "falling");
-        animation.AddClip(wakBackAnimationClip, "walkingback");
-        animation.AddClip(throwAnimationClip, "throwing");
 
         animation.wrapMode = WrapMode.Loop;
         animation.AddClip(walkAnimationClip, "walking");
-        animation.Stop();
+        animation.AddClip(idleAnimationClip, "idleing");
+        animation.AddClip(fallAnimationClip, "falling");
+        animation.AddClip(wakBackAnimationClip, "walkingback");
+
+        animation.wrapMode = WrapMode.Once;
+        animation.AddClip(throwAnimationClip, "throwing");
+
+
+        
     }
 
     void Start()
@@ -69,8 +72,6 @@ public class AlbertController2 : MonoBehaviour {
     private GameObject poof;
     void Update()
     {
-
-
         if (Input.GetKeyDown(KeyCode.A) ) { LetterBulletname = "a_projectilePrefab"; }
            if (Input.GetKeyDown(KeyCode.B)) { LetterBulletname = "b_projectilePrefab"; }
            if (Input.GetKeyDown(KeyCode.C)) { LetterBulletname = "c_projectilePrefab"; }
@@ -99,6 +100,220 @@ public class AlbertController2 : MonoBehaviour {
            if (Input.GetKeyDown(KeyCode.Z)) { LetterBulletname = "z_projectilePrefab"; }
 
 
+         /*
+        if (Input.GetKeyDown("space"))
+        {
+           // animation.CrossFade("throwing");
+            StartCoroutine(DoThrow());
+        }
+       // moveabout();
+        */
+SwitchCaseMove ( gettirrection());
+
+
+
+//Debug.Log(x.ToString());
+    }
+
+    void SwitchCaseMove(int x) {
+        float coef = 0.05f;
+        switch (x)
+        {
+            case 1:
+                mytransform.Translate(Vector3.right * -1 * coef, Space.World);
+                mytransform.Translate(Vector3.forward * -1 * coef);
+                Quaternion target = Quaternion.Euler(0, -1, 0);
+                transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+
+                break;
+            case 2:
+                mytransform.Translate(Vector3.forward * -1 * coef);
+                animation.CrossFade("walkingback");
+                break;
+            case 3:
+                mytransform.Translate(Vector3.right * 1 * coef, Space.World);
+                mytransform.Translate(Vector3.forward * -1 * coef);
+                Quaternion target1 = Quaternion.Euler(0, 1, 0);
+                transform.rotation = Quaternion.Slerp(transform.rotation, target1, Time.deltaTime * smooth);
+                break;
+            case 4:
+                mytransform.Translate(Vector3.right * -1 * coef, Space.World);
+                Quaternion target2 = Quaternion.Euler(0, -1, 0);
+                transform.rotation = Quaternion.Slerp(transform.rotation, target2, Time.deltaTime * smooth);
+                break;
+
+            case 6:
+                mytransform.Translate(Vector3.right * 1 * coef, Space.World);
+                Quaternion target3 = Quaternion.Euler(0, 1, 0);
+                transform.rotation = Quaternion.Slerp(transform.rotation, target3, Time.deltaTime * smooth);
+                break;
+            case 7:
+                mytransform.Translate(Vector3.right * 1 * coef, Space.World);
+                mytransform.Translate(Vector3.forward * -1 * coef);
+                Quaternion target4 = Quaternion.Euler(0, -1, 0);
+                transform.rotation = Quaternion.Slerp(transform.rotation, target4, Time.deltaTime * smooth);
+                break;
+            case 8:
+                mytransform.Translate(Vector3.forward * 1 * coef);
+                break;
+            case 9:
+                mytransform.Translate(Vector3.right * 1 * coef, Space.World);
+                mytransform.Translate(Vector3.forward * 1 * coef);
+                Quaternion target5 = Quaternion.Euler(0, 1, 0);
+                transform.rotation = Quaternion.Slerp(transform.rotation, target5, Time.deltaTime * smooth);
+
+                break;
+            default:
+                animation.Stop("walking");
+                break;
+        }
+    
+    
+    }
+    IEnumerator DoThrow()
+    {
+        animation.Stop("walking");
+        animation.CrossFade("throwing");
+        yield return new WaitForSeconds(animation["throwing"].length);
+
+    }
+
+
+
+    int gettirrection(){
+
+        int dir=0;
+        float leftRight = Input.GetAxis("Horizontal") * PlayerSpeed * Time.deltaTime;
+        float forwardBackward = Input.GetAxis("Vertical") * PlayerSpeed * Time.deltaTime;
+
+        //9
+        if (leftRight > 0 && forwardBackward > 0) { dir = 9; }
+        else
+        if (leftRight < 0 && forwardBackward > 0) { dir = 7; }
+        else
+        if (leftRight > 0 && forwardBackward < 0) { dir = 3; }
+        else
+        if (leftRight < 0 && forwardBackward < 0) { dir = 1; }
+        else
+        if (forwardBackward > 0) { dir = 8; }
+        else
+        if (forwardBackward < 0) { dir = 2; }
+        else
+        if (leftRight < 0) { dir = 4; }
+        else
+        if (leftRight > 0) { dir = 6; }
+
+
+
+        return dir;
+    }
+    
+    void moveabout() {
+        animation.CrossFade("walking");
+        float tiltAroundz = Input.GetAxis("Horizontal") * tiltAngle;
+
+        float leftRight = Input.GetAxis("Horizontal") * PlayerSpeed * Time.deltaTime;
+        float forwardBackward = Input.GetAxis("Vertical") * PlayerSpeed * Time.deltaTime;
+        
+        Debug.Log("leftright " + leftRight.ToString() + "    updown "+forwardBackward );
+        if (forwardBackward > 0) {
+            mytransform.Translate(Vector3.forward * forwardBackward);
+        }
+        if (forwardBackward < 0) {
+            mytransform.Translate(Vector3.forward * forwardBackward);
+            animation.CrossFade("walkingback");
+        }
+        if (leftRight < 0) {
+            mytransform.Translate(Vector3.right * leftRight, Space.World);
+            //tilting
+//            float tiltAroundz = Input.GetAxis("Horizontal") * tiltAngle;
+            Quaternion target = Quaternion.Euler(0, tiltAroundz, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+
+        }
+        if (leftRight > 0) {
+            mytransform.Translate(Vector3.right * leftRight, Space.World);
+            //tilting
+          //  float tiltAroundz = Input.GetAxis("Horizontal") * tiltAngle;
+            Quaternion target = Quaternion.Euler(0, tiltAroundz, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+       
+        }
+
+
+        //if ( forwardBackward != 0){mytransform.Translate(Vector3.forward * forwardBackward);}
+
+            /*
+        else
+            if (leftRight != 0 || forwardBackward != 0)
+            {
+                if (forwardBackward < 0)
+                    animation.CrossFade("walkingback");
+                else
+                    animation.CrossFade("walking");
+                // Move the player
+                mytransform.Translate(Vector3.right * leftRight, Space.World);
+                mytransform.Translate(Vector3.forward * forwardBackward);
+
+                //tilting
+                float tiltAroundz = Input.GetAxis("Horizontal") * tiltAngle;
+                Quaternion target = Quaternion.Euler(0, tiltAroundz, 0);
+                transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+            }
+            else
+            {
+                animation.CrossFade("walking");
+                //animation.CrossFade("idleing");
+                //mytransform.Translate(Vector3.back * 0.018f);
+            }
+
+
+        if (mytransform.position.x <= -7f)
+            mytransform.position = new Vector3(-7f, transform.position.y, transform.position.z);
+        else if (mytransform.position.x >= 7)
+            mytransform.position = new Vector3(7f, transform.position.y, transform.position.z);
+
+        // up and down player movement limitation
+        if (mytransform.position.z > 6f)
+            mytransform.position = new Vector3(gameObject.transform.position.x, transform.position.y, 6f);
+        else if (mytransform.position.z < -4.9f)
+            mytransform.position = new Vector3(gameObject.transform.position.x, transform.position.y, -4.9f);
+         * */
+
+    }
+
+    void oldupdate()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.A)) { LetterBulletname = "a_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.B)) { LetterBulletname = "b_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.C)) { LetterBulletname = "c_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.D)) { LetterBulletname = "d_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.E)) { LetterBulletname = "e_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.F)) { LetterBulletname = "f_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.G)) { LetterBulletname = "g_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.H)) { LetterBulletname = "h_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.I)) { LetterBulletname = "i_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.J)) { LetterBulletname = "j_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.K)) { LetterBulletname = "k_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.L)) { LetterBulletname = "l_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.M)) { LetterBulletname = "m_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.N)) { LetterBulletname = "n_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.O)) { LetterBulletname = "o_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.P)) { LetterBulletname = "p_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.Q)) { LetterBulletname = "q_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.R)) { LetterBulletname = "r_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.S)) { LetterBulletname = "s_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.T)) { LetterBulletname = "t_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.U)) { LetterBulletname = "u_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.V)) { LetterBulletname = "v_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.W)) { LetterBulletname = "w_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.X)) { LetterBulletname = "x_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.Y)) { LetterBulletname = "y_projectilePrefab"; }
+        if (Input.GetKeyDown(KeyCode.Z)) { LetterBulletname = "z_projectilePrefab"; }
+
+
 
 
         if (Input.GetKeyDown("space"))
@@ -108,9 +323,9 @@ public class AlbertController2 : MonoBehaviour {
 
             if (!LetterMode)
             {
-               // animation.CrossFade("throwing");
+                // animation.CrossFade("throwing");
                 // Fire projectile
-                
+
                 poof = Instantiate(ProjectilePrefab, position, this.transform.rotation) as GameObject;
                 poof.rigidbody.AddForce(transform.forward * 1000.0f);
             }
@@ -125,16 +340,17 @@ public class AlbertController2 : MonoBehaviour {
 
                     // Debug.Log(" letname is "+LetterBulletname);%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
                     //&&&&&&&&&&&&&&&&&&&&&&&&& GARENTEE A SHOT OF LETTER... if ( >0) only shoots if you have that lettter 
-                    if (Context.PlayerInventory.GetLetterCount(LettercChosen)  > 0 ) {
+                    if (Context.PlayerInventory.GetLetterCount(LettercChosen) > 0)
+                    {
                         poof = Instantiate(Resources.Load("LettesProjectile/" + LetterBulletname), position, Quaternion.Euler(-90, 0, 0)) as GameObject;
                         poof.GetComponent<LetterProjectileScript>().isactive = true;// I can't change this here
                         poof.rigidbody.AddForce(transform.forward * 1000.0f);
                         Context.PlayerInventory.take_letterAway(LettercChosen);
                     }
-                 
+
 
                 }
-           
+
             }
 
         }
@@ -145,33 +361,33 @@ public class AlbertController2 : MonoBehaviour {
         {
 
             //slw speed
-            mytransform.Translate(Vector3.right * leftRight /2 , Space.World);
+            mytransform.Translate(Vector3.right * leftRight / 2, Space.World);
             mytransform.Translate(Vector3.forward * forwardBackward);
 
         }
 
         else
-        if (leftRight != 0 || forwardBackward != 0)
-        {
-            if (forwardBackward<0)
-                animation.CrossFade("walkingback");
+            if (leftRight != 0 || forwardBackward != 0)
+            {
+                if (forwardBackward < 0)
+                    animation.CrossFade("walkingback");
+                else
+                    animation.CrossFade("walking");
+                // Move the player
+                mytransform.Translate(Vector3.right * leftRight, Space.World);
+                mytransform.Translate(Vector3.forward * forwardBackward);
+
+                //tilting
+                float tiltAroundz = Input.GetAxis("Horizontal") * tiltAngle;
+                Quaternion target = Quaternion.Euler(0, tiltAroundz, 0);
+                transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+            }
             else
-            animation.CrossFade("walking");
-            // Move the player
-            mytransform.Translate(Vector3.right * leftRight, Space.World);
-            mytransform.Translate(Vector3.forward * forwardBackward);
-
-            //tilting
-            float tiltAroundz = Input.GetAxis("Horizontal") * tiltAngle;
-            Quaternion target = Quaternion.Euler(0, tiltAroundz, 0);
-            transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
-        }
-        else
-        {
-            animation.CrossFade("walking");
-            //animation.CrossFade("idleing");
-            //mytransform.Translate(Vector3.back * 0.018f);
-        }
+            {
+                animation.CrossFade("walking");
+                //animation.CrossFade("idleing");
+                //mytransform.Translate(Vector3.back * 0.018f);
+            }
 
 
         if (mytransform.position.x <= -7f)
@@ -187,7 +403,6 @@ public class AlbertController2 : MonoBehaviour {
 
 
     }
-
 
     void OnTriggerEnter(Collider otherObj)
     {
