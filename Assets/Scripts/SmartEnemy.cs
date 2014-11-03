1627ea175dbbfa6f4ccde4559222a0073b752f4c
Paul <pdpman88@gmail.com>
2014-11-03 15:21:15 -0500

    begin work on side to side movement

--- Assets/Scripts/SmartEnemy.cs
+++ Assets/Scripts/SmartEnemy.cs
@@ -3,6 +3,17 @@ using System.Collections;
 
 public class SmartEnemy : Enemy {
 
+    // vars to control sine wave movement
+    public Transform target1;
+    public Transform target2;
+
+    private float amplitudeX = 10f;
+    private float amplitudeZ = 5f;
+    private float omegaX = 0.1f;
+    private float omegaZ = 0.1f;
+
+    private bool reachedMiddle = false;
+
 	/*// Use this for initialization
 	void Start () {
 	
@@ -27,21 +38,33 @@ public class SmartEnemy : Enemy {
                 transform.Translate(new Vector3(-0.5f, 0f, -1f) * atm, Space.World);
                 break;
             case 3:
-                SinMove(atm);
+                SinMovePrep(atm);
                 break;
         }
     }
 
-    private void SinMove(float atm)
+    private void SinMovePrep(float atm)
     {
-        if (transform.position.z <= 1.5f)
+        if (reachedMiddle)
+        {
+            SinMove(atm);
+        }
+        else if (transform.position.z <= 1.5f)
         {
             //sinusoidal patrol
-            transform.Translate(new Vector3(1f, 0, 0) * atm, Space.World);
+            //transform.Translate(new Vector3(1f, 0, 0) * atm, Space.World);
+            reachedMiddle = true;
         }
         else
         {
             transform.Translate(Vector3.back * atm, Space.World);
         }
+    }
+
+    private void SinMove(float atm)
+    {
+        float x = amplitudeX * Mathf.Cos(atm);
+        float z = Mathf.Abs(amplitudeZ * Mathf.Sin(atm));
+        transform.localPosition = new Vector3(x, 0, z);
     }
 }
