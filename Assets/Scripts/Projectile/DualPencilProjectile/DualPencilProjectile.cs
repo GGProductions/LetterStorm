cfc27cb27affe121dfa79f7f42549a31bafb31c1
nablam <lamribenn@gmail.com>
2014-11-16 09:51:10 -0500

    pre jeannie poweruos

--- Assets/Scripts/Projectile/DualPencilProjectile/DualPencilProjectile.cs
+++ Assets/Scripts/Projectile/DualPencilProjectile/DualPencilProjectile.cs
@@ -4,22 +4,22 @@ using System.Collections;
 public class DualPencilProjectile : MonoBehaviour
 {
 
-    #region Private Variables ---------------------------------------------
-    private Transform Pencil1;
-    private Transform Pencil2;
-    #endregion Private Variables ------------------------------------------
+	#region Private Variables ---------------------------------------------
+	private Transform Pencil1;
+	private Transform Pencil2;
+	#endregion Private Variables ------------------------------------------
 
-    // Use this for initialization
+	// Use this for initialization
 	void Start () {
-        //Pencil1 = this.transform.FindChild("pencil1");
-        //Pencil2 = this.transform.FindChild("pencil2");
+		//Pencil1 = this.transform.FindChild("pencil1");
+		//Pencil2 = this.transform.FindChild("pencil2");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (this.transform.childCount <= 0)
-            Destroy(gameObject);
-        //Pencil1.Rotate(new Vector3(0, 0, 2) * 20f * Time.deltaTime);
-        //Pencil2.Rotate(new Vector3(0, 0, 2) * 20f * Time.deltaTime);
+		if (this.transform.childCount <= 0)
+			Destroy(gameObject);
+		//Pencil1.Rotate(new Vector3(0, 0, 2) * 20f * Time.deltaTime);
+		//Pencil2.Rotate(new Vector3(0, 0, 2) * 20f * Time.deltaTime);
 	}
 }
