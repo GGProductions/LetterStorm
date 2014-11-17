7175488999925b5d838656a5ccf899a89a4ee20b
jamraygo <jamraygo@bu.edu>
2014-11-17 17:09:43 +0100

    graphs win scene and fixed lessons manage bug

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -8,10 +8,16 @@ public class Win : MonoBehaviour
 	//JR Texture for the background in the scene
     public Texture backgroundTexture;
 
+	//JR Title Texture
+	public Texture titleTexture;
+	
     void OnGUI()
     {
 		//JR Calls up background texture which is a .png file inside textures folder
         GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), backgroundTexture);
+		
+		//JR Calls up title texture which is a .png file inside textures folder
+        GUI.DrawTexture(new Rect(Screen.width / 2 - 120 , Screen.height / 2 - 100, 240, 60), titleTexture);
 
 		//JR if pressed load Next Level
         if(GUI.Button(new Rect(Screen.width / 2 - 40 , Screen.height / 2 + 25, 80, 20), "Level 1"))
